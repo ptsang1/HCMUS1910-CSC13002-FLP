07d73ce4468a7f254b87b7dd02359911679ed49d
ptsang1 <ptsang.nth@gmail.com>
2019-12-20 19:16:22 +0700

    update signIn.html, signUp.html

--- HomNayAnGiWeb/src/main/webapp/app.py
+++ HomNayAnGiWeb/src/main/webapp/app.py
@@ -14,5 +14,9 @@ def loadAddNewPost():
 def loadSigninPage():
     return render_template("login.html")
 
+@app.route('/signup')
+def loadSignupPage():
+    return render_template("signup.html")
+
 if __name__ == '__main__':
     app.run()
